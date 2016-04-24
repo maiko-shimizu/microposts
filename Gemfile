79f6866a1c7627f28162e15e4992b6c99a764c64
maiko_shimizu <chobineco@msn.com>
2016-04-24 00:16:43 +0000

    add profile_edit

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ gem 'bcrypt', '~> 3.1.7'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# デバッガ
+gem 'pry-byebug', group: :development
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,12 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
   def new
     @user = User.new
   end
   
   def show
-    @user = User.find(params[:id])
-  end
-  
-  def new
-    @user = User.new
   end
   
   def create
@@ -21,9 +19,31 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      #保存に成功した場合はユーザーページへリダイレクト
+      flash[:success] = "プロフィールを更新しました"
+      redirect_to @user
+    else
+      #保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :area, :password, :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def correct_user
+    redirect_to root_path unless current_user == @user 
   end
 end
