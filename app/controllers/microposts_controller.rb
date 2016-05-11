37703a7c0cac95d225611fa058d46be5bd71352e
maiko_shimizu <chobineco@msn.com>
2016-05-11 03:23:29 +0000

    revision1

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,6 +7,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @user = current_user
       render 'static_pages/home'
     end
   end
